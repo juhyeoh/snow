ad2d57eeea1f0979bbd7980e5c5714362777db20
juhye0102 <juhye0102@naver.com>
2024-02-05 09:02:39 +0900

    [fix]

--- snow_data/make_list.py
+++ snow_data/make_list.py
@@ -1,29 +1,31 @@
-from docx import Document
+import csv
 
-docx_file = 'snow_data/stnIds_list.docx'
+csv_file = 'snow_data/stnIds_list.csv'
 
-def col_value(docx_file, col_nm):
-    doc = Document(docx_file)
+def col_value(csv_file, col_nm):
     stn_val = []
 
-    for table in doc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                if cell.text.strip() == col_nm:
-                    index = row.cells.index(cell)
-                    for row in table.rows:
-                        value = row.cells[index].text.strip()
-                        stn_val.append(value)
-                    print(stn_val)
-                    return stn_val
-                
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        headers = reader.fieldnames
+
+        if col_nm not in headers:
+            print(f"does not exist {col_nm}.")
+            return stn_val
+
+        for row in reader:
+            value = row[col_nm].strip()
+            stn_val.append(value)
+
+    return stn_val
+
 def merge_col(col1_values, col4_values):
     if col1_values and col4_values:
         col1_values[-1] += " " + col4_values[0]
     return col1_values
 
-col1_values = col_value(docx_file, 'col1')
-col4_values = col_value(docx_file, 'col4')
+col1_values = col_value(csv_file, '지점1')
+col4_values = col_value(csv_file, '지점2')
 
 merged_values = merge_col(col1_values, col4_values)
 print(merged_values)
