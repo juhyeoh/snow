7de356b6642912f7c8c4b082ad52e0172951e9d3
juhye0102 <juhye0102@naver.com>
2024-03-18 08:53:55 +0900

    remove: operation_db 함수

--- snow_data/main.py
+++ snow_data/main.py
@@ -1,59 +1,6 @@
-import json
-import requests
-from datetime import datetime, timedelta
 from sqlalchemy.orm import sessionmaker
-from config_setting import create_db_engine
-from make_list import merged_data
-from config_setting import config_info, SnowApiData, Base
-
-def operation_db(session):
-    api_config = config_info()
-    url = api_config['api_end']
-
-    fir_date = (datetime.now() - timedelta(days=7)).strftime('%Y%m%d')
-    sec_date = (datetime.now() - timedelta(days=1)).strftime('%Y%m%d')
-
-    for stn_id, stn_nm in merged_data:
-        try:
-            params = {
-                'serviceKey': api_config['serviceKey'],
-                'pageNo': 1,
-                'numOfRows': 7,
-                'dataType': 'JSON',
-                'dataCd': 'ASOS',
-                'dateCd': 'DAY',
-                'startDt': fir_date,
-                'endDt': sec_date,
-                'stnIds': stn_id
-            }
-            response = requests.get(url, params=params)
-            data = response.json().get('response')
-            # print(data)
-            
-            if data and 'body' in data:
-                items = data['body'].get('items')
-                for item in items['item']:
-                    tm = item['tm']
-                    format_date = datetime.strptime(tm, '%Y-%m-%d')
-                    date = format_date.strftime('%Y-%m-%d')
-                    
-                    ddMes_str = item['ddMes']
-                    ddMes = float(ddMes_str) if ddMes_str else 0.0
-                    ddMefs_str = item['ddMefs']
-                    ddMefs = float(ddMefs_str) if ddMefs_str else 0.0
-                                        
-                    res_data = {
-                        'stnIds': stn_id,
-                        'stnNm': stn_nm,
-                        'date': date,
-                        'ddMes': ddMes,
-                        'ddMefs': ddMefs
-                    }
-                    session.add(SnowApiData(**res_data))
-                
-        except requests.exceptions.RequestException as e:
-            print(f"{e}")
-    session.commit()
+from config_setting import create_db_engine, Base
+from operation_db import oper_db
     
 
 def main():
@@ -62,7 +9,7 @@ def main():
     session = Session()
 
     Base.metadata.create_all(engine)
-    operation_db(session)
+    oper_db(session)
 
     session.close()
 
