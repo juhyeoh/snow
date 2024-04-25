08b57121ac9d5accfa1637fc6c18af4828782b39
juhye0102 <juhye0102@naver.com>
2024-04-25 10:03:24 +0900

    chore: ruff

--- snow_data/make_list.py
+++ snow_data/make_list.py
@@ -1,12 +1,13 @@
 import csv
 
-csv_file = 'c:/Users/yjwon-test/Desktop/asos/snow_data/stnIds_list.csv'
+csv_file = "C:/Users/yjwon-test/Desktop/2024/Python_2024/asos/snow_data"
+
 
 def col_value(csv_file, col_nm):
     stn_val = []
 
-    with open(csv_file, 'r', encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter='\t')
+    with open(csv_file, "r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter="\t")
         headers = next(reader)
 
         if col_nm not in headers:
@@ -21,19 +22,21 @@ def col_value(csv_file, col_nm):
 
     return stn_val
 
+
 def merge_id_col(col1_values, col4_values):
     merged_id_values = col1_values + col4_values[:-5]
     return merged_id_values
+
 
 def merge_nm_col(col2_values, col5_values):
     merged_nm_values = col2_values + col5_values[:-5]
     return merged_nm_values
 
 
-col1_values = col_value(csv_file, '지점1')
-col2_values = col_value(csv_file, '지점명1')
-col4_values = col_value(csv_file, '지점2')
-col5_values = col_value(csv_file, '지점명2')
+col1_values = col_value(csv_file, "지점1")
+col2_values = col_value(csv_file, "지점명1")
+col4_values = col_value(csv_file, "지점2")
+col5_values = col_value(csv_file, "지점명2")
 
 merged_id_values = merge_id_col(col1_values, col4_values)
 merged_nm_values = merge_nm_col(col2_values, col5_values)
